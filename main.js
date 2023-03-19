3684cf8af328b6a7642851cedafa85500d63af0f
Wendell <wtomimbang@msn.com>
2023-03-18 22:23:23 -0400

    Initial Commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let firstName = "Wendell";
 let lastName = "Tomimbang";
-let age = 5;
-alert ("My name is " + firstname + lastName.);
-alert ("I am "  + age + "years old");
+let age = 65;
+alert ("My name is " + firstName + " " + lastName + ".");
+alert ("I am "  + age + " " + "years old.");
 console.log ("I am having so much fun in the class.")
